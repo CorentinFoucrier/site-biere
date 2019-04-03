14d359530e8b773740a9f32e6439ae305928b62e
CorentinFoucrier <foucrier.corentin@gmail.com>
2019-04-03 23:09:10 +0200

    navbar flex

--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <header class="row mb-3">
+
 	<img id="bannière" src="images/brasseur.jpg" alt="brasseur" class="d-none d-lg-block" />
-		<nav class="col-12 d-flex">
-			<ul class="list-unstyled d-flex my-2 text-underline">
-				<li class="pr-3"><a href="index.php">Présentation</a></li>
-				<li class="pr-3"><a href="produit.php">Produit</a></li>
-				<li class="pr-3"><a href="autre-produits.php">Autres produits</a></li>
+
+		<nav class="col-12">
+			<ul class="list-unstyled h-100 d-flex align-items-center justify-content-around">
+				<li><a href="index.php">Présentation</a></li>
+				<li><a href="produit.php">Produit</a></li>
+				<li><a href="autre-produits.php">Autres produits</a></li>
 			</ul>
 		</nav>
+
 </header>
