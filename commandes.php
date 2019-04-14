dbefcb23302efed498d6daa9eb920ed60ae01599
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-14 12:14:17 +0200

    formulaire responsive

--- commandes.php
+++ commandes.php
@@ -30,7 +30,7 @@ include('beerArray.php');
 					</div>
 
 					<div class="form-row">
-						<div class="form-group col-3">
+						<div class="form-group col-12 col-md-3">
 							<label for="tel">Téléphone</label>
 							<input type="text" class="form-control" name="tel">
 						</div>
@@ -41,47 +41,48 @@ include('beerArray.php');
 					</div>
 
 					<div class="form-row">
-						<div class="form-group col">
+						<div class="form-group col-12 col-lg">
 							<label for="adresse">Adresse</label>
 							<input type="text" class="form-control" name="adresse">
 						</div>
-						<div class="form-group col-3">
+						<div class="form-group col-12 col-lg-3">
 							<label for="ville">Ville</label>
 							<input type="text" class="form-control" name="ville">
 						</div>
-						<div class="form-group col-2">
+						<div class="form-group col-12 col-lg-2">
 							<label for="pays">Pays</label>
 							<input type="text" class="form-control" name="pays">
 						</div>
-						<div class="form-group col-2">
+						<div class="form-group col-12 col-lg-2">
 							<label for="codePostal">Code postale</label>
 							<input type="text" class="form-control" name="codePostal">
 						</div>
 					</div>
-					
-					<table class="table table-striped mt-5">
-						<thead>
-							<tr>
-								<th scope="col">Nom de la bière</th>
-								<th scope="col">Prix HT</th>
-								<th scope="col">Prix TTC</th>
-								<th scope="col">Quantité</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php for ($i=0; $i < count($beerArray); $i++) { ?>
-							<tr id="<?= ($i+1) ?>">
-								<th scope="row"><?= $beerArray[$i][0] ?></th>
-								<td>€ <?= number_format($beerArray[$i][3], 2, ',', '.'); ?></td>
-								<td>€ <?= number_format($beerArray[$i][3]*1.2, 2, ',', '.'); ?></td>
-								<td>
-									<input class="form-control" onclick="pomme(<?= ($i+1) ?>)" type="number" value="0" min="0" name="<?= 'beerName'.$i ?>">
-									<input id="prixInitial<?= ($i+1) ?>" type="hidden" value="<?= $beerArray[$i][3] ?>">
-								</td>
-							</tr>
-							<?php } ?>
-						</tbody>
-					</table>
+					<div class="table-responsive">
+						<table class="table table-striped mt-5">
+							<thead>
+								<tr>
+									<th scope="col">Nom de la bière</th>
+									<th scope="col">Prix HT</th>
+									<th scope="col">Prix TTC</th>
+									<th scope="col">Quantité</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php for ($i=0; $i < count($beerArray); $i++) { ?>
+								<tr id="<?= ($i+1) ?>">
+									<th scope="row"><?= $beerArray[$i][0] ?></th>
+									<td>€ <?= number_format($beerArray[$i][3], 2, ',', '.'); ?></td>
+									<td>€ <?= number_format($beerArray[$i][3]*1.2, 2, ',', '.'); ?></td>
+									<td>
+										<input class="form-control" onclick="pomme(<?= ($i+1) ?>)" type="number" value="0" min="0" name="<?= 'beerName'.$i ?>">
+										<input id="prixInitial<?= ($i+1) ?>" type="hidden" value="<?= $beerArray[$i][3] ?>">
+									</td>
+								</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
 					<button type="submit" class="btn btn-light float-left">Envoyer</button>
 				</fieldset>
 			</form>
