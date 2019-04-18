722deeee17e76269de4dcdee8cf072b05afab469
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-18 12:00:44 +0200

    connexion bdd + remplissage beerArray dans une table

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+include 'beerArray.php';
+include 'db.php';
+
+$sql = 'INSERT INTO biere (titre, img, description, prix) VALUES (:titre, :img, :description, :prix)';
+
+foreach ($beerArray as $element) { 
+$statement = $pdo->prepare($sql);
+$result = $statement->execute([
+	':titre' 		=> $element[0],
+	':img'			=> $element[1],
+	':description' 	=> $element[2],
+	':prix' 		=> $element[3]
+]);
+}
+
+?>
