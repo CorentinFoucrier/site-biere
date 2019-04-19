d13a8e5a34ee2535bad0a6e807412839478bc20d
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-19 10:50:32 +0200

    passage en bdd l'affichage des bière

--- autre-produits.php
+++ autre-produits.php
@@ -1,8 +1,7 @@
 <?php 
 
 	session_start();
-
-	include('beerArray.php');
+	require 'db.php';
 
 	function add3dots($string, $repl, $limit) 
 	{
@@ -16,6 +15,11 @@
 	  }
 	}
 
+	$reqBiere = "SELECT * FROM biere";
+	$statement = $pdo->query($reqBiere);
+	$bieres = $statement->fetchAll();
+
+
 ?>
 
 <!DOCTYPE html>
@@ -28,24 +32,24 @@
 		<div id="oppacity-bg" class="container">
 			<?php include('header.php'); ?>
 			<section class="row">
-				<?php foreach ($beerArray as $element) { ?>
+				<?php foreach ($bieres as $biere) { ?>
 				<div class="col-md-4 offset-md-1 mb-5">
 					<div class="row box">
 						<!-- HEADER -->
 						<div class="col-12">
-							<h3 class="my-3 text-center text-uppercase"><?php echo $element[0]; ?></h3>
+							<h3 class="my-3 text-center text-uppercase"><?php echo $biere['titre']; ?></h3>
 						</div>
 						<!-- IMG -->
 						<div class="col-12 box-borders">
-							<img class="my-3 mx-auto d-block" src="<?php echo $element[1]; ?>" height="250" />
+							<img class="my-3 mx-auto d-block" src="<?php echo $biere['img']; ?>" height="250" />
 						</div>
 						<!-- Description -->
 						<div class="col-12 ">
-							<p class="my-1"><?php echo add3dots($element[2], "...", 100); ?></p>
+							<p class="my-1"><?php echo add3dots($biere['description'], "...", 100); ?></p>
 						</div>
 						<!-- Prix -->
 						<div class="col-12">
-							<h5 class="my-3 text-center"><?php echo number_format($element[3]*1.2, 2, ',', '.'); ?>€ TTC</h5>
+							<h5 class="my-3 text-center"><?php echo number_format($biere['prix']*1.2, 2, ',', '.'); ?>€ TTC</h5>
 						</div>
 					</div>
 				</div>
