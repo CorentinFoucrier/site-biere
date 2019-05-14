d6174079234d5dc531e625dc2a0f0676f20a97c5
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-05-14 15:34:46 +0200

    envoie mail + alert flash

--- assets/testing/test2.php
+++ assets/testing/test2.php
@@ -1,42 +1,7 @@
 <?php
-if (!empty($_POST['sendForm1'])) {
-	$nom = htmlentities($_POST['nom']);
-	if (!empty($nom)) {
-		echo "FORM 1 ".$_POST['nom'];
-	}else{
-		echo 'le FORM1 est vide !';
-	}
-}
-
-if (!empty($_POST['sendForm2'])) {
-	$nom = htmlentities($_POST['age']);
-	if (!empty($nom)) {
-		echo "FORM 2 ".$_POST['age'];
-	}else{
-		echo 'le FORM 2 est vide !';
-	}
-}
+$nextWeek = time() + (7 * 24 * 60 * 60);
+// 7 jours; 24 heures; 60 minutes; 60 secondes
+echo 'Aujourd\'hui :       '. date('Y-m-d') ."\n";
+echo 'Semaine prochaine : '. date('Y-m-d', $nextWeek) ."\n";
+echo time();
 ?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>un titre</title>
-</head>
-	<body>
-		<h2 id="<?php echo "FORM 1" ?>"><?php echo "FORM 1" ?></h2>
-
-		<form method="post" action="" name="form1">
-			<input type="text" name="nom" value="Corentin">
-			<button type="submit" name="sendForm1" value="sendForm1" for="form1">Envoyer</button>
-		</form>
-
-		<h2>Form 2</h2>
-
-		<form method="post" action="" name="form2">
-			<input type="text" name="age" value="Age: 22">
-			<button type="submit" name="sendForm2" value="sendForm2" for="form2">Envoyer</button>
-		</form>
-	</body>
-</html>
--- login.php
+++ login.php
@@ -1,8 +1,40 @@
 <?php
 	require_once 'includes/function.php';
+	require_once 'assets/vendor/autoload.php';
 	require_once 'db.php';
 	if (isConnect()) { //Si connecter goto index
 		header('Location: index.php');
+	}
+	if (isset($_GET['t'])) {
+		require_once 'db.php';
+		$reqUser = 'SELECT * FROM users WHERE id = ?';
+		$state = $pdo->prepare($reqUser);
+		$state->execute([$_GET['id']]);
+		$user = $state->fetch();
+		if ($user) {
+			/* si token est validé alors on passe en get pour afficher l'alert */
+			if ($_GET['t'] == $user['token'] && $user['tokenCreatedAt'] < ($user['tokenCreatedAt']+300)) {
+				require_once 'db.php';
+				$sql = "UPDATE `users` SET `tokenVerify` = 'true' WHERE `users`.`id` = :id";
+				$statement = $pdo->prepare($sql);
+				$result = $statement->execute([':id' => $_GET['id']]);
+				if (session_status() != PHP_SESSION_ACTIVE){
+					session_start();
+				}
+				$_SESSION['tvalid'] = 'true';
+			} else {
+				/* sinon alert pas valide */
+				if (session_status() != PHP_SESSION_ACTIVE){
+					session_start();
+				}
+				$_SESSION['tvalid'] = 'false';
+			}
+		}else{
+			if (session_status() != PHP_SESSION_ACTIVE){
+				session_start();
+			}
+			$_SESSION['tvalid'] = 'false';
+		}
 	}
 	#################################
 	############# LOGIN #############
@@ -20,21 +52,36 @@
 			$user = $statement->fetch();
 			/* Si $user est true alors on verif que le mdp est pareil qu'en BDD */
 			if ($user) {
-				if (password_verify($password, $user["password"])){
-					$_SESSION["id"] = $user['id'];
-					header("Location: index.php");
+				if (password_verify($password, $user['password'])){
+					/* Si token est true on le connecte et redirige vers index */
+					if ($user['tokenVerify'] == 'true') {
+						if (session_status() != PHP_SESSION_ACTIVE){
+							session_start();
+						}
+						$_SESSION['id'] = $user['id'];
+						header('Location: index.php');
+					} else {
+						if (session_status() != PHP_SESSION_ACTIVE){
+							session_start();
+						}
+						$_SESSION['tverify'] = 'false';
+					}
 				}else{
-					header('Location: index.php');
+					// TODO : afficher erreur flash
+					header('Location: login.php');
 				}
 			}else{
-				header('Location: index.php');
+				// TODO : afficher erreur flash
+				header('Location: login.php');
 			}
 		}else{
 			if (empty($username)) {
-				header('Location: index.php');
+				// TODO : afficher erreur flash
+				header('Location: login.php');
 			}
 			if (empty($password)) {
-				header('Location: index.php');
+				// TODO : afficher erreur flash
+				header('Location: login.php');
 			}
 		}
 	}
@@ -67,14 +114,26 @@
 
 			/* Si $user est false (l'utilisateur et email existe pas) */
 			if (!$user) {
-				var_dump($user);
 				/* Verif que le mdp soit dans le format désiré */
 				if (strlen($password) <= 10 && strlen($password) >= 5) { // Format à déposer dans la condition
 					/* Si mot de passe identiques = true */
 					if ($password === $passwordConfirmed) {
 						$passwordhash = password_hash($password, PASSWORD_BCRYPT);
 						require_once 'db.php';
-						$sql = "INSERT INTO `users` (`nom`, `prenom`, `telephone`, `email`, `adresse`, `ville`, `pays`, `codePostal`, `username`, `password`) VALUES (:nom, :prenom, :telephone, :email, :adresse, :ville, :pays, :codePostal, :username, :password)";
+
+						$token = generateRandomString();
+						$tokenCreatedAt = time();
+
+						$sql = "INSERT INTO `users` (
+								`nom`, `prenom`, `telephone`, 
+								`email`, `adresse`, `ville`, 
+								`pays`, `codePostal`, `username`, 
+								`password`, `token`, `tokenCreatedAt`) 
+								VALUES (
+								:nom, :prenom, :telephone, 
+								:email, :adresse, :ville, 
+								:pays, :codePostal, :username, 
+								:password, :token, :tokenCreatedAt)";
 						$statement = $pdo->prepare($sql);
 						$result = $statement->execute([
 							':nom' => $nom,
@@ -86,7 +145,9 @@
 							':pays' => $pays,
 							':codePostal' => $codePostal,
 							':username' => $username,
-							':password' => $passwordhash
+							':password' => $passwordhash,
+							':token' => $token,
+							':tokenCreatedAt' => $tokenCreatedAt
 						]);
 						if ($result) {
 							/* Tout s'est bien passé */
@@ -96,9 +157,16 @@
 							$state->execute([$username]);
 							$user = $state->fetch();
 							if ($user) {
-								session_start();
-								$_SESSION['id'] = $user['id'];
-								header("Location: index.php");
+								sendMail('foucrier.corentin@gmail.com', [$email], 'Confirmation bread beer shop !', '<html><body>Pour activer votre compte cliquez <a href="http://github.local/site-biere/login.php?t='.$token.'&id='.$user['id'].'">ici</a> !</body></html>', 'Lien d\'activation => http://github.local/site-biere/login.php?t='.$token.'&id='.$user['id'].'');
+								// $sendMailReturn = var_dump(sendMail());
+								//if ($numSent == 1) {
+			                        if (session_status() != PHP_SESSION_ACTIVE){
+										session_start();
+									}
+									$_SESSION['cw'] = 'true';
+								//}else{
+								//	die('Erreur envoie mail');
+								//}
 							}else{
 								die('Erreur SQL');
 							}
@@ -122,6 +190,32 @@
 			die('Tout est vide !');
 		}
 	}
+	#################################
+	###### RESEND EMAIL TOKEN #######
+	#################################
+	if(!empty($_POST['resendEmailForm'])){
+		$email = htmlentities($_POST['email']);
+		if (!empty($email)) {
+			require_once 'db.php';
+
+			$token = generateRandomString();
+			$tokenCreatedAt = time();
+
+			$sql = "INSERT INTO `users` (`token`, `tokenCreatedAt`) VALUES (:token, :tokenCreatedAt";
+			$statement = $pdo->prepare($sql);
+			$result = $statement->execute([
+				':token' => $token,
+				':tokenCreatedAt' => $tokenCreatedAt
+			]);
+			if ($user) {
+				sendMail('foucrier.corentin@gmail.com', [$email], 'Confirmation bread beer shop !', '<html><body>Pour activer votre compte cliquez <a href="http://github.local/site-biere/login.php?t='.$token.'&id='.$user['id'].'">ici</a> !</body></html>', 'Lien d\'activation => http://github.local/site-biere/login.php?t='.$token.'&id='.$user['id'].'');
+				if (session_status() != PHP_SESSION_ACTIVE){
+					session_start();
+				}
+				$_SESSION['resendEmail'] = 'true';
+			}
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -132,8 +226,42 @@
 	<body>
 		<?php require 'includes/header.php'; ?>
 		<div class="container">
-			<div class="row mt-5">
-				<?php if (!isset($_GET['pwd'])) : ?>
+			<?php if (isset($_SESSION['cw']) && ($_SESSION['cw'] == 'true')) : ?>
+			<div class="alert alert-info mt-3" role="alert">
+				Inscription prise en compte. Un email de confirmation vous a été envoyé.
+			</div>
+			<?php unset($_SESSION['cw']); ?>
+			<?php endif ?>
+
+			<?php if (isset($_SESSION['tvalid']) && ($_SESSION['tvalid'] == 'true')) : ?>
+			<div class="alert alert-success mt-3" role="alert">
+				Inscription prise en compte. Vous pouvez vous connecter !
+			</div>
+			<?php session_unset($_SESSION['tvalid']); ?>
+			<?php endif ?>
+
+			<?php if (isset($_SESSION['tvalid']) && ($_SESSION['tvalid'] == 'false')) : ?>
+			<div class="alert alert-danger mt-3" role="alert">
+				Token invalide ! Pour renvoyer l'email <a href="login.php?resendEmail" class="alert-link">ici</a> !
+			</div>
+			<?php session_unset($_SESSION['tvalid']); ?>
+			<?php endif ?>
+
+			<?php if (isset($_SESSION['resendEmail']) && ($_SESSION['resendEmail'] == 'true')) : ?>
+			<div class="alert alert-success mt-3" role="alert">
+				Nouvelle email envoyer cliquez <a href="login.php" class="alert-link">ici</a> pour vous connecter !
+			</div>
+			<?php session_unset($_SESSION['resendEmail']); ?>
+			<?php endif ?>
+
+			<?php if (isset($_SESSION['tverify']) && ($_SESSION['tverify'] == 'false')) : ?>
+			<div class="alert alert-danger mt-3" role="alert">
+				Votre compte n'a pas été activé, veuillez vérifier vos emails !
+			</div>
+			<?php session_unset($_SESSION['tverify']); ?>
+			<?php endif ?>
+			<div class="row mt-4">
+				<?php if (!isset($_GET['pwd']) && !isset($_GET['resendEmail'])) : ?>
 				<div class="col-md-6 px-md-5 mb-5">
 					<div class="loginForm">
 						<div class="pt-3 pl-3">
@@ -270,6 +398,7 @@
 					</div>
 				</div>
 				<?php endif ?>
+
 				<?php if (isset($_GET['pwd'])) : ?>
 				<div class="col-md-6 offset-md-3 col-12 px-md-5 mb-5">
 					<div class="loginForm">
@@ -299,6 +428,37 @@
 					</div>
 				</div>
 				<?php endif ?>
+
+				<?php if (isset($_GET['resendEmail'])) : ?>
+				<div class="col-md-6 offset-md-3 col-12 px-md-5 mb-5">
+					<div class="loginForm">
+						<div class="pt-3 pl-3">
+							<h1>Renvoyer l'email de confirmation</h1>
+						</div>
+						<div>
+							<form class="p-4" name="resendEmail">
+								<div class="form-group">
+									<label for="email">Votre Email</label> 
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<div class="input-group-text">
+												<i class="fa fa-at"></i>
+											</div>
+										</div> 
+										<input id="email" name="email" placeholder="example@example.com" type="text" class="form-control" required="required">
+									</div>
+								</div>
+
+								<div class="form-group">
+									<button type="submit" name="resendEmailForm" value="resendEmailForm" for="resendEmail" class="btn btn-success">Revoyer</button>
+									<a href="login.php"><span class="btn btn-secondary">Retour</span></a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<?php endif ?>
+
 			</div>
 		</div>
 		<?php require 'includes/footer.php'; ?>
